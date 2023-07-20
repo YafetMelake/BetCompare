2861d36757a39a2c54b235e5f04307e1e6c3a7d3
YafetMelake <91352437+YafetMelake@users.noreply.github.com>
2023-07-20 11:40:01 +0100

    still base no message needed

---
+++ LoginCheck/DetailsInput.py
@@ -0,0 +1,18 @@
+import requests
+import sqlite3
+from RapidApiEmailCheck import get_email
+from StoringEmails.db import make_account
+
+def authentication():
+    while True: 
+        email = input("Please enter your Email: ")
+        valid_email = get_email(email)
+        while not valid_email:
+            email = input("Please enter another Email as this is not Valid: ")
+            valid_email = get_email(email)
+
+        print("Please now enter a password")
+        valid_password = input("Password:")
+
+        if make_account(valid_email, valid_password):
+            break
