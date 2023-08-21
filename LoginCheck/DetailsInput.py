f3ce6dc8364e5f5f801a4afd596b08554effc6dc
YafetMelake <91352437+YafetMelake@users.noreply.github.com>
2023-08-21 09:04:42 +0100

    uncommented and added description

--- LoginCheck/DetailsInput.py
+++ LoginCheck/DetailsInput.py
@@ -1,19 +1,22 @@
-# from LoginCheck.RegExEmailCheck import *
-# from LoginCheck.StoringEmails import *
+'''authentication function prompts user to enter email and uses the check_email and make_account
+    function to validate and verify the details entered and if so, adds the details to the database'''
 
-# def authentication(sportsdbfile):
-#     while True:
-#         email = input("Please enter your Email: ")
-#         valid_email = check_email(email)
-#         while not valid_email:
-#             email = input("Please enter another Email as this is not Valid: ")
-#             valid_email = check_email(email)
+from LoginCheck.RegExEmailCheck import *
+from LoginCheck.StoringEmails import *
 
-#         print(
-#             "Please now enter a password, make it as secure or as weak as you want..."
-#         )
-#         valid_password = input("Password:")
+def authentication(sportsdbfile):
+    while True:
+        email = input("Please enter your Email: ")
+        valid_email = check_email(email)
+        while not valid_email:
+            email = input("Please enter another Email as this is not Valid: ")
+            valid_email = check_email(email)
 
-#         if make_account(email, valid_password, sportsdbfile):
-#             break
+        print(
+            "Please now enter a password, make it as secure or as weak as you want..."
+        )
+        valid_password = input("Password:")
+
+        if make_account(email, valid_password, sportsdbfile):
+            break
 
