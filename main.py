cb86441b2dbdcb5ba068e8b7f87307ba1e11ff9f
YafetMelake <91352437+YafetMelake@users.noreply.github.com>
2023-08-23 13:29:20 +0100

    changed the main.py to a function so that it can be called in app.py do I only have to schedule app.py which can then call and receive the most up-to-date data to display in the frontend

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# main.py
 sportsdbfile = "C:/Users/yafet/Documents/GitHub/BetCompare/sports_oddyy.db"
-
 from LoginCheck.DetailsInput import authentication
 from LoginCheck.RapidApiSport import sportssoddss
 
-# authentication(sportsdbfile)
-sportssoddss(sportsdbfile)
+# Function to set up initial data for the sports odds application.
+# This function is designed to be called from app.py to ensure that app.py displays the most recent data.
+# Scheduling app.py to run this function can keep the data up-to-date without deploying the entire application.
+def setup_initial_data():
+
+    # authentication(sportsdbfile)
+    sportssoddss(sportsdbfile)
